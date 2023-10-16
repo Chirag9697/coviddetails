1a476321267d86d20efeef0c4e222947752234e6
Chirag Ranasaria <90066944+Chirag9697@users.noreply.github.com>
2023-10-16 11:40:48 +0545

    fixed profile styles

--- frontend/src/Pages/Profileset/MyProfile.js
+++ frontend/src/Pages/Profileset/MyProfile.js
@@ -44,73 +44,92 @@ const MyProfile = () => {
    
     <Heading color="var(--chakra-colors-blue-500);" fontSize="2xl" style={{marginRight:"170px"}}>
       Your Details
+
     </Heading>
-    <SimpleGrid columns={2} columnGap={3} rowGap={6} justifyItems="space-between" style={{ width: "500px" }}>
-      <GridItem  style={{marginLeft:"70px"}}>
-      <Flex direction="column">
-        <Flex align="center">
+    {/* <SimpleGrid columns={2} columnGap={3} rowGap={6} justifyItems="space-between" style={{ width: "500px" }}> */}
+      {/* <GridItem  style={{marginLeft:"70px"}}> */}
+      <Flex direction="row" style={{flexWrap:"wrap",justifyContent:"space-between",width:"20vw", height:"50vh"}}>
+        <Flex align="center" direction="column">
+          <Flex>
           <Box>
             <FaUser size={20} style={{ backgroundColor: 'transparent' }}/>
           </Box>
+
           <Heading as="h5" size="md" fontSize="lg" align="center" marginLeft="10px">
-            First Name:
+            First Name
           </Heading>
+          </Flex>
           <span color="red" fontSize="lg" style={{ marginLeft: '10px' }}>{data.firstName}</span>
         </Flex>
 
-        <Flex align="center">
+        <Flex align="center" direction="column">
+          <Flex>
+
           <Box>
             <FaUser size={20} style={{ backgroundColor: 'transparent' }}/>
           </Box>
           <Heading as="h5" size="md" fontSize="lg" align="center" marginLeft="10px">
-            Last Name:
+            Last Name
           </Heading>
+          </Flex>
           <span color="red" fontSize="lg" style={{ marginLeft: '10px' }}>{data.lastName}</span>
         </Flex>
 
-        <Flex align="center">
+        <Flex align="center" direction="column">
+          <Flex>
+
           <Box>
           <MdLocationOn size={20} style={{ backgroundColor: 'transparent' }}/>
           </Box>
           <Heading as="h5" size="md" fontSize="lg" align="center" marginLeft="10px">
-            Address:
+            Address
           </Heading>
-          <span color="red" fontSize="lg" style={{ marginLeft: '10px' }}>{data.address}</span>
+          </Flex>
+          <span color="red" fontSize="lg" style={{ marginLeft: '10px',textAlign:"center" }}>{data.address}</span>
         </Flex>
 
-        <Flex align="center">
+        <Flex align="center" direction="column">
+          <Flex>
+
           <Box>
           <MdPhone size={20} style={{ backgroundColor: 'transparent' }}/>
           </Box>
           <Heading as="h5" size="md" fontSize="lg" align="center" marginLeft="10px">
-              Phone:
+              Phone
           </Heading>
+          </Flex>
           <span color="red" fontSize="lg" style={{ marginLeft: '10px' }}>{data.phone}</span>
         </Flex>
 
-        <Flex align="center">
+        <Flex align="center" direction="column">
+          <Flex>
+
           <Box>
           <FaUser size={20} style={{ backgroundColor: 'transparent' }}/>
           </Box>
           <Heading as="h5" size="md" fontSize="lg" align="center" marginLeft="10px">
-              Gender:
+              Gender
           </Heading>
+          </Flex>
           <span color="red" fontSize="lg" style={{ marginLeft: '10px' }}>{data.gender}</span>
         </Flex>
 
-        <Flex align="center">
+        <Flex align="center" direction="column">
+          <Flex>
+
           <Box>
           <MdEvent size={20} style={{ backgroundColor: 'transparent' }}/>
           </Box>
           <Heading as="h5" size="md" fontSize="lg" align="center" marginLeft="10px">
-              DOB:
+              DOB
           </Heading>
+          </Flex>
           <span color="red" fontSize="lg" style={{ marginLeft: '10px' }}>{data.dob}</span>
         </Flex>        
       </Flex>
         
-      </GridItem>
-    </SimpleGrid>
+      {/* </GridItem> */}
+    {/* </SimpleGrid> */}
   
   </VStack>
   
